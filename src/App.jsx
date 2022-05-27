c15994e74fc9a35f64555d27c192b666e0430cf8
pankajnandi <pkandpal28@gmail.com>
2022-05-27 19:02:32 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import  Todo  from "./components/Todo";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="outterScreen">
+      <div className="App">
+          <Todo  /> 
+          <h2>Made by Pankaj Kandpal 💜</h2>
+       </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import "./Todo.css"
+
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+
+import axios from 'axios';
+import { useEffect } from 'react';
+
+
+function TodoInput({ togleCheck,getTodo}) {
+
+    const [input1, setInput1] = useState("");
+    const [input2, setInput2] = useState("");
+
+    const clearField = () =>{
+        setInput1("");
+        setInput2("")
+    }
+
+    const addTodo = (input1,input2) =>{
+        let data = {title : input1, body : input2 , status : false};
+        axios.post("https://mighty-temple-00792.herokuapp.com/todo",data).then(getTodo);
+        togleCheck()
+        // setTodo([...todo,data])
+        // console.log(todo);
+    }
+
+
+  return (
+
+            
+                    <div className='InputBox'>
+                                <input type="text" className='inputTitle' value={input1}
+                                placeholder="Add Title..."
+                                onChange={(e) => {
+                                        setInput1(e.target.value)
+                                    }}/>
+
+                                <input type="text" className='inputBody' value={input2}
+                                onChange={(e) => {
+                                        setInput2(e.target.value)
+                                    }}
+                                placeholder="Add Task..." />
+
+                                
+                            
+                                <Button className='addBtn' variant="contained"
+                                    onClick={() => {
+                                        addTodo(input1,input2)
+                                        clearField();
+                                    }}
+                                endIcon={<SendIcon />}>
+                                    Add
+                                </Button>
+
+                
+                </div>
+    
+            
+  )
+  
+ 
+}
+
+export default TodoInput;
